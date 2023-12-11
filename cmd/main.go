d717281a09fe5bad0091247958dd0937f15d8318
JamesBond <introbond.dev@gmail.com>
2023-12-12 06:32:02 +0700

    Refactor graceful shutdown handling

--- cmd/main.go
+++ cmd/main.go
@@ -17,21 +17,7 @@ func main() {
 
 	ctx, cancel := context.WithCancel(context.Background())
 
-	// Handle graceful shutdown using a goroutine
-	go func() {
-		sigCh := make(chan os.Signal, 1)
-		signal.Notify(sigCh, os.Interrupt)
-
-		<-sigCh
-
-		fmt.Println("\nReceived interrupt signal. Initiating graceful shutdown...")
-		cancel()
-
-		wg.Wait()
-
-		fmt.Println("Shutdown complete.")
-		os.Exit(0)
-	}()
+	go handleGracefulShutdown(cancel, &wg)
 
 	// Start HTTP server in a goroutine
 	go func() {
@@ -62,4 +48,20 @@ func main() {
 	}
 
 	wg.Wait()
+}
+
+// Handle graceful shutdown using a goroutine
+func handleGracefulShutdown(cancel context.CancelFunc, wg *sync.WaitGroup) {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+
+	<-sigCh
+
+	fmt.Println("\nReceived interrupt signal. Initiating graceful shutdown...")
+	cancel()
+
+	wg.Wait()
+
+	fmt.Println("Shutdown complete.")
+	os.Exit(0)
 }
