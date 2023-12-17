3b274a25267a1dfa52e2340901c81ca87bcd8c54
JamesBond <introbond.dev@gmail.com>
2023-12-17 13:36:41 +0700

    Add graceful shutdown handling

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,6 +1,12 @@
 package util
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+)
 
 const (
 	ColorReset  = "\033[0m"
@@ -16,4 +22,19 @@ const (
 
 func PrintColored(message string, colorCode string) {
 	fmt.Printf("%s%s%s\n", colorCode, message, ColorReset)
+}
+
+func HandleGracefulShutdown(cancel context.CancelFunc, wg *sync.WaitGroup) {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+
+	<-sigCh
+
+	fmt.Println("\nReceived interrupt signal. Initiating graceful shutdown...")
+	cancel()
+
+	wg.Wait()
+
+	fmt.Println("Shutdown complete.")
+	os.Exit(0)
 }
