b1c1df9193573908f7b183363b3418ee834f7ff2
JamesBond <introbond.dev@gmail.com>
2023-12-17 13:36:53 +0700

    Refactor main.go and remove unused code

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"os"
-	"os/signal"
 	"sync"
 
 	"github.com/bondzai/invoker/internal/api"
-	"github.com/bondzai/invoker/internal/mock"
-	"github.com/bondzai/invoker/internal/task"
+	"github.com/bondzai/invoker/internal/util"
 )
 
 func main() {
@@ -17,36 +13,23 @@ func main() {
 
 	ctx, cancel := context.WithCancel(context.Background())
 
-	go handleGracefulShutdown(cancel, &wg)
+	go util.HandleGracefulShutdown(cancel, &wg)
 
 	server := api.NewHttpServer()
 	go server.Start(ctx)
 
-	taskManagers := *task.NewTaskManagers()
-	taskFromDB := mock.Tasks
+	// taskManagers := *task.NewTaskManagers()
+	// taskFromDB := mock.Tasks
 
-	for _, t := range *taskFromDB {
-		wg.Add(1)
-		go func(task task.Task) {
-			defer wg.Done()
-			taskManagers[task.Type].Start(ctx, task, &wg, nil)
-		}(t)
-	}
-
-	wg.Wait()
-}
-
-func handleGracefulShutdown(cancel context.CancelFunc, wg *sync.WaitGroup) {
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, os.Interrupt)
-
-	<-sigCh
-
-	fmt.Println("\nReceived interrupt signal. Initiating graceful shutdown...")
-	cancel()
+	// for _, t := range *taskFromDB {
+	// 	wg.Add(1)
+	// 	go func(task task.Task) {
+	// 		defer wg.Done()
+	// 		taskManagers[task.Type].Start(ctx, task, &wg, nil)
+	// 	}(t)
+	// }
 
 	wg.Wait()
 
-	fmt.Println("Shutdown complete.")
-	os.Exit(0)
+	select {}
 }
