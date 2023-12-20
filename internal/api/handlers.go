9234fd5243c9466e2d3dbac88867656f2a152f57
JamesBond <introbond.dev@gmail.com>
2023-12-20 07:33:33 +0700

    Refactor response method names in handlers.go

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -18,13 +18,13 @@ func (s *Server) GetTaskHandler(w http.ResponseWriter, r *http.Request) {
 	idStr := r.URL.Query().Get("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		s.sendResponseMessage(w, http.StatusBadRequest, "Invalid request payload")
+		s.response(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 
 	task, ok := s.Scheduler.Read(id)
 	if !ok {
-		s.sendResponseMessage(w, http.StatusNotFound, "Task ID not exists")
+		s.response(w, http.StatusNotFound, "Task ID not exists")
 		return
 	}
 
@@ -36,54 +36,54 @@ func (s *Server) GetTaskHandler(w http.ResponseWriter, r *http.Request) {
 func (s *Server) CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
 	var newTask scheduler.Task
 	if err := json.NewDecoder(r.Body).Decode(&newTask); err != nil {
-		s.sendResponseMessage(w, http.StatusBadRequest, "Invalid request payload")
+		s.response(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 
 	if _, ok := s.Scheduler.Read(newTask.ID); ok {
-		s.sendResponseMessage(w, http.StatusConflict, "Task with the same ID already exists")
+		s.response(w, http.StatusConflict, "Task with the same ID already exists")
 		return
 	}
 
 	s.Scheduler.Create(&newTask)
-	s.sendResponseMessage(w, http.StatusCreated, "Task created successfully")
+	s.response(w, http.StatusCreated, "Task created successfully")
 }
 
 func (s *Server) UpdateTaskHandler(w http.ResponseWriter, r *http.Request) {
 	idStr := r.URL.Query().Get("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		s.sendResponseMessage(w, http.StatusBadRequest, "Invalid task ID")
+		s.response(w, http.StatusBadRequest, "Invalid task ID")
 		return
 	}
 
 	var updatedTask scheduler.Task
 	if err := json.NewDecoder(r.Body).Decode(&updatedTask); err != nil {
-		s.sendResponseMessage(w, http.StatusBadRequest, "Invalid request payload")
+		s.response(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 
 	if _, ok := s.Scheduler.Read(id); !ok {
-		s.sendResponseMessage(w, http.StatusNotFound, "Task ID not exists")
+		s.response(w, http.StatusNotFound, "Task ID not exists")
 		return
 	}
 
 	s.Scheduler.Update(id, &updatedTask)
-	s.sendResponseMessage(w, http.StatusOK, "Task updated successfully")
+	s.response(w, http.StatusOK, "Task updated successfully")
 }
 
 func (s *Server) DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
 	idStr := r.URL.Query().Get("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		s.sendResponseMessage(w, http.StatusBadRequest, "Invalid task ID")
+		s.response(w, http.StatusBadRequest, "Invalid task ID")
 		return
 	}
 
 	if !s.Scheduler.Delete(id) {
-		s.sendResponseMessage(w, http.StatusNotFound, "Task ID not exists")
+		s.response(w, http.StatusNotFound, "Task ID not exists")
 		return
 	}
 
-	s.sendResponseMessage(w, http.StatusOK, "Task deleted successfully")
+	s.response(w, http.StatusOK, "Task deleted successfully")
 }
