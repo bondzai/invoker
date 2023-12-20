492288637f549214448c6b7129e652218c543611
JamesBond <introbond.dev@gmail.com>
2023-12-20 08:27:13 +0700

    Refactor scheduler.MockTasks function

---
+++ internal/scheduler/mock.go
@@ -0,0 +1,54 @@
+package scheduler
+
+import (
+	"fmt"
+	"time"
+)
+
+func MockTasks() map[int]*Task {
+	mode := "static"
+	tasks := make(map[int]*Task)
+
+	if mode == "static" {
+		tasks[1] = &Task{
+			ID:       1,
+			Type:     IntervalTask,
+			Name:     "Task1",
+			Interval: time.Duration(60) * time.Second,
+			CronExpr: []string{"* * * * *"},
+			Disabled: false,
+		}
+
+		tasks[2] = &Task{
+			ID:       2,
+			Type:     CronTask,
+			Name:     "Task2",
+			Interval: time.Duration(4) * time.Second,
+			CronExpr: []string{"*/2 * * * *", "* * * * *"},
+			Disabled: false,
+		}
+	}
+
+	if mode == "dynamic" {
+		for i := 1; i <= 100000; i++ {
+			var taskType TaskType
+			if i%2 == 0 {
+				taskType = IntervalTask
+			} else {
+				taskType = CronTask
+			}
+
+			task := &Task{
+				ID:       i,
+				Type:     taskType,
+				Name:     fmt.Sprintf("Task%d", i),
+				Interval: time.Duration(i) * time.Second,
+				CronExpr: []string{"* * * * *"},
+				Disabled: false,
+			}
+			tasks[i] = task
+		}
+	}
+
+	return tasks
+}
